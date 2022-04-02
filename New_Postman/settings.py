561a54f0d8b71d9e0d00526ff71585b5d19d8488
pulkit-github123 <pulkitsinghai55@gmail.com>
2022-04-02 17:35:23 +0300

    committing

--- New_Postman/settings.py
+++ New_Postman/settings.py
@@ -124,4 +124,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = BASE_DIR/"staticfiles"
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
